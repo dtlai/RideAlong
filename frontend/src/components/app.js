253034fb66a0431765c2c019df534613ba0edd88
david <davidlai9@gmail.com>
2021-06-14 14:45:06 -0700

    finished frontend_auth

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -15,7 +15,7 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
-import NavBarContainer from "./nav/navbar_container";
+// import NavBarContainer from "./nav/navbar_container";
 
 import MainPage from "./main/mainpage";
 import LoginFormContainer from "./session/login_form_container";

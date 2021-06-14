a94a7c41e195eff9d227009bb6aa8a04cfcc882e
Michael Lau <lau.michael2@gmail.com>
2021-06-14 11:42:53 -0700

    Add mainpage scss

--- frontend/src/components/main/mainpage.js
+++ frontend/src/components/main/mainpage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './mainpage.scss';
 
 class MainPage extends React.Component {
   render() {
     return (
       <div>
         <div>
-          
+
         </div>
         <footer>
           Copyright &copy; 2021 RideAlong

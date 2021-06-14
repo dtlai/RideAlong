d31368562b56be59f667971268cee84cc6df6a83
david <davidlai9@gmail.com>
2021-06-14 13:41:16 -0700

    routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -13,13 +13,13 @@ router.get("/", (req, res) => {
     .catch((err) => res.status(404).json({ nopostsfound: "No posts found" }));
 });
 
-router.get("/user/:user_id", (req, res) => {
-  Post.find({ user: req.params.user_id })
-    .then((posts) => res.json(posts))
-    .catch((err) =>
-      res.status(404).json({ nopostsfound: "No posts found from that user" })
-    );
-});
+// router.get("/user/:user_id", (req, res) => {
+//   Post.find({ user: req.params.user_id })
+//     .then((posts) => res.json(posts))
+//     .catch((err) =>
+//       res.status(404).json({ nopostsfound: "No posts found from that user" })
+//     );
+// });
 
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)

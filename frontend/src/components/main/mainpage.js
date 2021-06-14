6f2a0df46f26a0ca54c7ecc7fd677c2806e803d9
Nathan Luu <nateluu28@gmail.com>
2021-06-14 15:33:25 -0700

    Modify Navbar styling

--- frontend/src/components/main/mainpage.js
+++ frontend/src/components/main/mainpage.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import NavBar from '../nav_bar.js/NavBar';
+import { FaGithub } from "react-icons/fa";
+
 import './mainpage.scss';
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-          <NavBar />
+      <div className="parent-main">
         <div className="main-splash">
-          <h3></h3>
+          <NavBar />
+          <div className="title-container">
+            <h1 className="title-splash">Get started today!</h1>
+          </div>
         </div>
         <footer>
           Copyright &copy; 2021 RideAlong
+          <div className="icon github">
+            <div className="tooltip">Github</div>
+            <FaGithub size={20} color={'black'}/>
+          </div>
         </footer>
       </div>
     )

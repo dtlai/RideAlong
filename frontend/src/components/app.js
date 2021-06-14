1552ab487feb3e5439fa07474f0190b4c7a584ed
david <davidlai9@gmail.com>
2021-06-14 14:30:29 -0700

    making frontend compons

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import MainPage from './main/mainpage';
+// import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+// import MainPage from './main/mainpage';
+
+// const App = () => (
+//  <div>
+//    <Switch>
+//      <Route exact path="/" component={MainPage}/>
+//    </Switch>
+//  </div>
+// )
+
+// export default App;
+
+import React from "react";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Switch, Route } from "react-router-dom";
+import NavBarContainer from "./nav/navbar_container";
+
+import MainPage from "./main/mainpage";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
- <div>
-   <Switch>
-     <Route exact path="/" component={MainPage}/>
-   </Switch>
- </div>
-)
+  <div>
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+  </div>
+);
 
 export default App;
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import session from "./session_reducer";
 
 const RootReducer = combineReducers({
-
+  session,
 });
 
 export default RootReducer;

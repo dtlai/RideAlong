8a78ed393b943b5900f918c5d4c18805b788b6e5
Linda Xiao <lindax28@hotmail.com>
2021-06-15 15:44:18 -0700

    Revert "Merge branch 'posts' of https://github.com/dtlai/RideAlong into posts"
    This reverts commit 5dade58157a444439585c6f34b67fc1980598be8, reversing
    changes made to ac451b862a12f8eeb58bd6eb4bdbcc492da7e866.

--- frontend/src/components/posts/post_box.js
+++ frontend/src/components/posts/post_box.js
@@ -4,25 +4,18 @@ import './post_box.scss';
 class PostBox extends React.Component {
   render() {
     let {post} = this.props
-    let startDate = post.createdAt.slice(0, 10);
-    let dateLeave = post.leaveDate.slice(0, 10);
     return (
       <div className="post-container">
         <div className="post-title-container">
-          <span className="post-title">{post.title}</span>
-          <span>{startDate}</span>
-        </div>
-        <div className="post-capacity-container">
-          <span>Seats: {post.capacity} </span>
-          <span>Seats left: {post.capacity - post.numPassengers}</span>
+          <h3>{post.title}</h3>
         </div>
         <div className="post-user-container">
-          Driver: {post.user}
+          User:&nbsp;{post.user}
         </div>
         <div className="post-locations-container">
-          <span>Leaving {post.startLocation} to {post.endLocation} on {dateLeave}</span>
+          <h3>From:&nbsp;{post.startLocation}</h3>
+          <h3>To:&nbsp;{post.endLocation}</h3>
         </div>
-        <div>Price range: ${post.price}</div>
       </div>
     )
   }

a900704815453785d60c2aa615d4107ddedc21f3
Michael Lau <lau.michael2@gmail.com>
2021-06-16 16:20:49 -0700

    Add post cards to main page

--- frontend/src/components/main/mainpage.js
+++ frontend/src/components/main/mainpage.js
@@ -1,18 +1,52 @@
 import React from 'react';
 // import { FaGithub } from "react-icons/fa";
 import NavBarContainer from '../nav_bar/nav_bar_container';
-
+import {withRouter} from 'react-router-dom';
 import './mainpage.scss';
+import Postcard from '../posts/post_card';
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(newState) {
+    this.setState({ posts: newState.posts })
+  }
+
+  onSubmit() {
+    this.props.history.push('/posts/create')
+  }
+
   render() {
     return (
       <div className="parent-main">
         <div className="main-splash">
           <NavBarContainer />
           <div className="title-container">
-            <h1 className="title-splash">Get started today!</h1>
+            <div className="title-splash">
+              <h3>
+                Begin a Trip!
+              </h3>
+              <button className="get-started-button" onClick={() => this.onSubmit()}>Create a Post</button>
+            </div>
           </div>
+        </div>
+        <div className="post-cards-container">
+          {this.state.posts.map(post => (
+            <Postcard key={post.id} post={post}/>
+          ))}
         </div>
         <footer>
           Copyright &copy; 2021 RideAlong
@@ -26,4 +60,4 @@ class MainPage extends React.Component {
   }
 }
 
-export default MainPage;
+export default withRouter(MainPage);
---
+++ frontend/src/components/posts/post_card.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './post_card.scss';
+import {withRouter} from 'react-router-dom'
+
+// const Postcard = ({post}) => {
+//   return (
+//     <div>
+//       <div className="postcard-box">
+//         <div className="card-img">
+
+//         </div>
+//         <div className="card-content">
+//           <div>
+//             {post.title}
+//           </div>
+//           <div className="postcard-description">
+//             {post.description}
+//           </div>
+//           <div>
+
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+class Postcard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.sendToPost = this.sendToPost.bind(this);
+  }
+
+  sendToPost() {
+    this.props.history.push(`/post/${this.props.post._id}`)
+  }
+
+  render() {
+    let {post} = this.props
+    return (
+      <div>
+        <div onClick={this.sendToPost} className="postcard-box">
+          <div className="card-img">
+
+          </div>
+          <div className="card-content">
+            <div>
+              {post.title}
+            </div>
+            <div className="postcard-description">
+              {post.description}
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    )  
+  }
+}
+
+export default withRouter(Postcard);

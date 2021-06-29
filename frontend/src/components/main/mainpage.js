e733050e3ac11c90f84244f30275c824ec42dbcf
david <davidlai9@gmail.com>
2021-06-29 12:36:08 -0700

    adding footer

--- frontend/src/components/main/mainpage.js
+++ frontend/src/components/main/mainpage.js
@@ -5,6 +5,7 @@ import './mainpage.scss';
 import Postcard from '../posts/post_card';
 import SearchBar from '../search/search_form';
 import { AiFillCar } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 
 class MainPage extends React.Component {
@@ -50,24 +51,55 @@ class MainPage extends React.Component {
             </div>
             <div className="title-splash">
               <h3>Start your journey!</h3>
-              <button className="get-started-button" onClick={() => this.onSubmit()}>Create a Trip</button>
+              <button
+                className="get-started-button"
+                onClick={() => this.onSubmit()}
+              >
+                Create a Trip
+              </button>
             </div>
           </div>
         </div>
-        <div className="card-header">Take a look at some current trips <AiFillCar className="card-header-car"/></div>
+        <div className="card-header">
+          Take a look at some current trips{" "}
+          <AiFillCar className="card-header-car" />
+        </div>
         <div className="post-cards-container">
-          {this.state.posts.map(post => (
-            <Postcard key={post.id} post={post}/>
-          )).slice(0, 6)}
+          {this.state.posts
+            .map((post) => <Postcard key={post.id} post={post} />)
+            .slice(0, 6)}
           <div>
-            <button onClick={this.sendToPosts} className="view-more-button">View more trips</button>
+            <button onClick={this.sendToPosts} className="view-more-button">
+              View more trips
+            </button>
           </div>
         </div>
         <footer>
-          Copyright &copy; 2021 RideAlong
+          <>
+            <div className="copyright">Copyright &copy; 2021 RideAlong</div>
+            <div className="menu-container">
+              <p>Menu</p>
+              <Link to="/profile">Profile</Link>
+              <Link to="/posts">Posts</Link>
+
+              <Link to="/posts/create">Create Post</Link>
+              <Link to="/">Home</Link>
+            </div>
+
+            <div className="follow-container">
+              <p>Follow</p>
+              <Link to="/about">About Us</Link>
+            </div>
+
+            <div className="get-started-container">
+              <p>Get Started</p>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          </>
         </footer>
       </div>
-    )
+    );
   }
 }
 
